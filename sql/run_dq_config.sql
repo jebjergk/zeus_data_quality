ab09dee5fadc97c9bbd86e12a2b779c8863b1a1e
jebjergk <kenneth.jebjerg@baaderbank.de>
2025-10-13 17:32:24 +0200

    chore: update /snapshots .p mirror (#109)

--- sql/run_dq_config.sql
+++ sql/run_dq_config.sql
@@ -1,7 +1,7 @@
--- RUN_DQ_CONFIG Stored Procedure
+-- DQ_RUN_CONFIG Stored Procedure
 -- To execute in Snowsight: open a worksheet, set your context, and run:
---   CALL RUN_DQ_CONFIG('<CONFIG_ID>');
-create or replace procedure RUN_DQ_CONFIG(CONFIG_ID STRING)
+--   CALL DQ_RUN_CONFIG('<CONFIG_ID>');
+create or replace procedure DQ_RUN_CONFIG(CONFIG_ID STRING)
 returns STRING
 language SQL
 as
