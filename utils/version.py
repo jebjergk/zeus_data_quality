8abf6e3c3ceab2d99fbc90c344eae609e0239a99
jebjergk <kenneth.jebjerg@baaderbank.de>
2025-11-12 21:28:31 +0100

    chore: update /snapshots .p mirror (#371)

--- utils/version.py
+++ utils/version.py
@@ -1,12 +1,11 @@
-import os
-import pathlib
+import os, pathlib
 
 
-def build_sha() -> str:
+def build_sha():
     p = pathlib.Path("snapshots/BUILD_SHA.txt")
     return p.read_text().strip() if p.exists() else os.getenv("GIT_SHA", "unknown")
 
 
-def build_time() -> str:
+def build_time():
     p = pathlib.Path("snapshots/BUILD_TIME_UTC.txt")
     return p.read_text().strip() if p.exists() else "unknown"
